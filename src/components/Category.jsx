2873922eadc6fb61f0db2e4a04813499da2f81cb
Ibuchukwu Umeano <ibumeano@gmail.com>
2023-12-16 17:17:02 +0100

    added hover effect on homepage

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,10 +5,10 @@ const Category = (props) => {
   return (
     <div
       key={id}
-      className="h-[350px] w-full child-div bg-no-repeat bg-cover bg-center flex justify-center items-center category-card"
+      className="h-[350px] w-full bg-no-repeat bg-cover bg-center flex justify-center items-center category-card"
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
-      <p className="p-3 cursor-pointer bg-[rgba(255,255,255,0.7)] rounded-sm flex flex-col justify-center items-center" >{title.toUpperCase()} <span className="underline text-sm">Shop now</span></p>
+      <p className="p-3 label cursor-pointer bg-[rgba(255,255,255,0.7)] rounded-sm flex flex-col justify-center items-center" >{title.toUpperCase()} <span className="underline text-sm">Shop now</span></p>
     </div>
   );
 };
