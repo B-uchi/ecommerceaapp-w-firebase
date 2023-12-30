24b2d66f105cef936485c36941ed48fc46db792e
Ibuchukwu Umeano <ibumeano@gmail.com>
2023-12-30 18:56:46 +0100

    merge trail error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import { setCurrentUser } from "./redux/reducers/userReducer/user.actions";
 import Homepage from "./pages/homepage/homepage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
